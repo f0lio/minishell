9c06141e7ef976b548ddfa66fc5f87ea9b1ea0ba
f0lio <36214338+f0lio@users.noreply.github.com>
2021-10-09 16:24:01 +0100

    fix SEGF on reset_data after repl

--- src/utils/destructors.c
+++ src/utils/destructors.c
@@ -3,14 +3,9 @@
 
 void    reset_data(ENV)
 {
-    // return;
-    
     safe_free ((void**)&env->input->line);
-    while (env->cmds_count >= 0)
-    {
+    while (--env->cmds_count >= 0)
         destroy_command(env->commands[env->cmds_count]);
-        env->cmds_count--;
-    }
     safe_free((void**)&env->commands);
     env->commands = NULL;
     env->input->i = 0;
@@ -19,15 +14,13 @@ void    reset_data(ENV)
 void    destroy_command(void *ptr)
 {
     t_command *cmd;
+ 
     cmd = ptr;
     if (!cmd)
         return ;
     safe_free((void**)&cmd->cmd);
-    while (cmd->tokens_count >= 0)
-    {
+    while (--cmd->tokens_count >= 0)
         destroy_token(cmd->tokens[cmd->tokens_count]);
-        cmd->tokens_count--;
-    }
     safe_free((void**)&cmd);
 }
 
@@ -38,7 +31,7 @@ void    destroy_token(void *ptr)
     token = ptr;
     if (token == NULL)
         return ;
-    // safe_free((void*)&token->tok);
+    safe_free((void*)&token->tok);
     safe_free((void*)&token->type);
     safe_free((void*)&token);
 }
